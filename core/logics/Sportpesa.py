f1628507509c5d5606c77e452fc06034b796d3c7
warrenwalt <warrenwalt19@gmail.com>
2024-06-17 18:02:17 +0300

    sportpesa progress. some bugs to be fixed

--- core/logics/Sportpesa.py
+++ core/logics/Sportpesa.py
@@ -1,4 +1,5 @@
 from time import time
+import pandas as pd
 
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
@@ -17,102 +18,70 @@ driver.maximize_window()
 
 # wait for page to load
 try:
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "event-row-container")))
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".event-row-details.event-markets-count-4")))
 
 except Exception as e:
     print(e)
     driver.close()
     exit()
 
-# get all elements with class name "event-row-container ng-scope"
+teams = []
+x12 = []
+btts = []
+dc = []
+
 
 matches = driver.find_elements(
-    By.CSS_SELECTOR, "div.event-row-container.ng-scope")
+    By.CSS_SELECTOR, '.event-row-details.event-markets-count-4')
 
-
-sure_bet_odds = []
-double_chance_odds = []
-over_under_odds = []
-
-for match in matches[:5]:
+for match in matches:
     # teams
-    teams_container = match.find_element(
-        By.CSS_SELECTOR, "div.event-details")
+    event_details = match.find_element(By.CLASS_NAME, 'event-names')
+    teams.append(event_details.text)
 
     # odds
-    odds_containers = match.find_elements(
-        By.CSS_SELECTOR, "div.event-market.market-3-way.event-market-availables-count-0.market-selections-3")
+    odds = match.find_elements(By.CLASS_NAME, 'event-market')
 
-    # 3-way
-    sure_bets = odds_containers[0].find_elements(
-        By.CSS_SELECTOR, "div.event-selection.ng-scope"
-    )
+    # 1x2
+    x12.append(odds[0].text)
 
-    team_odds = {
-        "bookmaker": "Sportpesa",
-        "match": teams_container.text.replace("\n", " v ").lower(),
-        "odds": []
-    }
+    # btts
+    # bt = odds[3].find_element(By.CLASS_NAME, 'ng-binding')
+    # print(bt.text)
+    # print('------------------')
+    btts.append(odds[3].text)
 
-    for sure_bet in sure_bets:
-        team_odds["odds"].append(sure_bet.text)
-
-    sure_bet_odds.append(team_odds)
-
-    # double chance
-    if len(odds_containers) < 2:
-        continue
-
-    # double_chance = odds_containers[1].find_elements(
-    #     By.CSS_SELECTOR, "div.event-selection.ng-scope"
-    # )
-
-    # team_odds_double = {
-    #     "bookmaker": "Sportpesa",
-    #     "match": teams_container.text.replace("\n", " v ").lower(),
-    #     "odds": []
-    # }
-
-    # for double_chance_bet in double_chance:
-    #     team_odds_double["odds"].append(double_chance_bet.text)
-
-    # double_chance_odds.append(team_odds_double)
-
-    # # over/under
-    # over_under = odds_containers[2].find_elements(
-    #     By.CSS_SELECTOR, "div.event-selection.ng-scope"
-    # )
-
-    # team_odds_over_under = {
-    #     "bookmaker": "Sportpesa",
-    #     "match": teams_container.text.replace("\n", " v ").lower(),
-    #     "odds": []
-    # }
-
-    # for over_under_bet in over_under:
-    #     team_odds_over_under["odds"].append(over_under_bet.text)
-
-    # over_under_odds.append(team_odds_over_under)
+    # dc
+    dc.append(odds[1].text)
 
 
-print(f"double chance odds🎯🎯 {sure_bet_odds}")
-print("==========================")
-print(f"over/under odds🎯🎯 {double_chance_odds[:3]}")
+driver.quit()
+
+'''CLEANING DATA'''
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 
-print(len(matches))
+dict_gambling = {'Teams': teams, '3-Way': x12, 'BTTS': btts, 'DC': dc}
+df = pd.DataFrame(dict_gambling)
+
+# replace space with v for teams col
+df['Teams'] = df['Teams'].str.replace('\n', ' v ').str.lower()
+
+# change 3-way to array of floats
+df['3-Way'] = df['3-Way'].str.split('\n')
+df['3-Way'] = df['3-Way'].apply(lambda x: [float(i)
+                                if isinstance(i, str) and len(x) > 1 else i for i in x])
+
+# change dc to array of floats
+df['DC'] = df['DC'].str.split('\n')
+df['DC'] = df['DC'].apply(
+    lambda x: [float(i) if isinstance(i, str) and len(x) > 1 else i for i in x])
 
 
-# time.sleep(3)
-
-driver.close()
-
-[
-    {'bookmaker': 'Sportpesa', 'match': '15/06/24 - 23:00 | id: 2754 v hfx wanderers fc v forge fc',
-        'odds': ['1.57', '1.25', '1.24']},
-    {'bookmaker': 'Sportpesa', 'match': '15/06/24 - 23:00 | id: 3076 v ponte preta v novorizontino sp', 'odds': [
-        '1.33', '1.36', '1.31']},
-    {'bookmaker': 'Sportpesa', 'match': '15/06/24 - 23:00 | id: 3099 v csa al v botafogo pb',
-        'odds': ['1.51', '1.21', '1.31']}
-]
+'''SAVE DATA'''
+df.to_csv('sportpesa.csv', index=False)
+# print(df)
+print(len(btts))
